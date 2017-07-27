cd737e5aa3ed4e80133bd146fc52e70f536aeb4e
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-07-27 17:03:42 -0400

    updated nav text

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,9 +12,9 @@ export default class NavBar extends Component {
     return (
       <div>
         <Menu pointing secondary>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+          <Menu.Item name='listings' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item name='saved listings' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+          <Menu.Item name='my profile' active={activeItem === 'messages'} onClick={this.handleItemClick} />
           <Menu.Menu position='right'>
             <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
           </Menu.Menu>
